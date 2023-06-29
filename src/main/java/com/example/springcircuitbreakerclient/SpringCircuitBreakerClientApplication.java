ffc57899512f29d1d9a84a37ca6df5bfcc5cfc8e
Siddharth Pathak <siddharthpathak415@gmail.com>
2023-06-29 16:11:22 -0700

    added RestController for client endpoint - read

--- src/main/java/com/example/springcircuitbreakerclient/SpringCircuitBreakerClientApplication.java
+++ src/main/java/com/example/springcircuitbreakerclient/SpringCircuitBreakerClientApplication.java
@@ -2,9 +2,22 @@ package com.example.springcircuitbreakerclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+
+@RestController
 @SpringBootApplication
-public class SpringCircuitBreakerClientApplication {
+public class  SpringCircuitBreakerClientApplication {
+
+    @RequestMapping("/read")
+    public Mono<String> Read(){
+        return WebClient.builder().build()
+                .get().uri("https://localhost:8090/suggested")
+                .retrieve().bodyToMono(String.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCircuitBreakerClientApplication.class, args);
