596992b3410184c2f199c57052cf07192272238d
Sid Pathak <siddharthpathak415@gmail.com>
2023-06-29 15:53:04 -0700

    init

---
+++ src/main/java/com/example/springcircuitbreakerclient/SpringCircuitBreakerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcircuitbreakerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCircuitBreakerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCircuitBreakerClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcircuitbreakerclient/SpringCircuitBreakerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcircuitbreakerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCircuitBreakerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
